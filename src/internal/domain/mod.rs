f612117f9cba6be4daf55a07bec3bf4aa38cfdac
TestardR <romain.rtestard@gmail.com>
2023-10-05 20:13:35 +0200

    feat(domain): add port repository

--- src/internal/domain/mod.rs
+++ src/internal/domain/mod.rs
@@ -1,3 +1,4 @@
 pub mod port;
 pub mod port_id;
 pub mod coordinates;
+pub mod port_repository;
---
+++ src/internal/domain/port_repository.rs
@@ -0,0 +1,12 @@
+use super::port::Port;
+use super::port_id::PortId;
+
+pub trait PortFinder {
+    fn find(&self, id: PortId) -> Port;
+}
+pub trait PortAdder {
+    fn add(&self, port: Port);
+}
+pub trait PortUpdater {
+    fn update(&self, port: Port);
+}
